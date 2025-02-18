1655b386079a7b9a75a2b3b8b8ec92d461235adf
arbtin <arbtin@gmail.com>
2025-02-17 21:00:45 -0600

    add tailwind and routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Person from "./person/Person.tsx";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './common/Home.tsx';
+import Person from './person/Person.tsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-        <Person/>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <BrowserRouter>
+            <nav className="flex bg-gray-800 h-12 w-72 p-4 items-center ">
+                <Link to="/">Home</Link> | <Link to="/person">Person</Link>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/person" element={<Person />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,7 +18,7 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   build: {
     outDir: 'build',
   },
